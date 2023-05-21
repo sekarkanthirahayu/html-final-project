917763d19db5f706eab0c14ae081d8b686eef511
sekarkanthirahayu <sekarkanthirahayu@gmail.com>
2023-05-21 14:59:36 +0700

    final project

---
+++ deployData.js
@@ -0,0 +1,13 @@
+/*
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Sekar Kanthi Rahayu'; // TODO: replace this
+const studentId = 'FE4750760'; // TODO: replace this
+const NetlifyDeployUrl = 'TODO: replace me'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
